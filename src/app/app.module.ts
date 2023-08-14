4420033b938f88dc9cad16ffbd13f40b22a8811c
RodrigoPablo <rodrigo.psilva@terceiroslinx.com.br>
2023-08-14 15:06:59 -0300

    realizado o html principal.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav'; // Importe o MatSidenavModule
+import { MatListModule } from '@angular/material/list'; // Importe o MatListModule
+import { MatToolbarModule } from '@angular/material/toolbar'; // Importe o MatToolbarModule
+
+
+
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +22,11 @@ import { AppMaterialModule } from './shared/app-material/app-material.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     AppMaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
