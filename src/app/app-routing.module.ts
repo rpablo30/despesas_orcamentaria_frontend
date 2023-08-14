9f940584121a0281cceec6f0d375421033b23100
RodrigoPablo <rodrigo.psilva@terceiroslinx.com.br>
2023-08-14 12:47:47 -0300

    Implementação da classe de serviço e dos erros.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'despesas', pathMatch: 'full' },
+  { path: 'despesas', loadChildren: () => import('./despesas/despesas.module').then(m => m.DespesasModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
