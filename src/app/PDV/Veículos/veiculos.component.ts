cd9707178d3e09658b43992b039625e34d7e33a3
Rodrigo Pablo <rpablo30@hotmail.com>
2023-09-14 14:05:41 -0300

    Ajustes para realizar a interação com o backend.

--- src/app/PDV/Veículos/veiculos.component.ts
+++ src/app/PDV/Veículos/veiculos.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Veiculos } from '../model/veiculos';
+import { PdvService } from '../services/pdv.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-veiculos',
@@ -8,25 +11,48 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class VeiculosComponent {
   veiculoForm: FormGroup;
+  veiculo: Veiculos = {
+    tipo: 'carro',
+    modelo: '',
+    anoDeFabricacao: 0,
+    marca: ''
+  };
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private pdvService: PdvService,
+    private router: Router
+  ) {
     this.veiculoForm = this.formBuilder.group({
       modelo: ['', Validators.required],
-      anoDeFabricacao: ['', [Validators.required, Validators.min(1900), Validators.max(2099)]],
+      anoDeFabricacao: ['', Validators.required],
       marca: ['', Validators.required],
       tipo: ['carro', Validators.required],
-      portas: ['2', Validators.required], // Valor padrão
-      passageiros: ['1', Validators.required] // Valor padrão
+      portas: ['2'], // Valor padrão
+      passageiros: ['1'] // Valor padrão
     });
   }
 
   onSubmit() {
     if (this.veiculoForm.valid) {
+      // Use o método .value do FormGroup para obter os valores do formulário
+      const formValues = this.veiculoForm.value;
+
+      // Copie os valores do formulário para o objeto veiculo
+      this.veiculo = { ...this.veiculo, ...formValues };
+
       
-      console.log('Dados do formulário:', this.veiculoForm.value);
-    } else {
-      
-      console.log('Formulário inválido');
+      this.pdvService.cadastrarVeiculo(this.veiculo).subscribe(
+        () => {
+          console.log('Veículo cadastrado com sucesso');
+          
+          this.router.navigate(['/veiculos']);
+        },
+        (error) => {
+          console.error('Erro ao cadastrar veículo:', error);
+         
+        }
+      );
     }
   }
 }
